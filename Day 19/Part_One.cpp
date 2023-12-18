969d4eaf052e09bdb865f5f108131b0ab965648c
Rahulram <89180961+Mr-Appu@users.noreply.github.com>
2023-12-18 14:28:16 +0530

    Day 18 : Part One

---
+++ Day 19/Part_One.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+const int MOD =  1e9 + 7;
+const int N = 1e5 + 5;
+const int INF = 1e10;
+
+/* is no pow of two or not o(1) */
+int is_power_of_2(int n)
+{
+    if(!(n&(n-1))) return 1;
+    else return 0;
+}
+ 
+bool cmp(const int a, const int b) { return a > b;}
+ 
+int powr(int a,int b) // log(b)
+{
+    int ans = 1;
+    while(b) { 
+        if(b&1) ans = (ans*a)%MOD;
+        a = (a*a) % MOD;
+        b = b>>1;
+    }
+    return ans;
+}
+ 
+int gcd(int a,int b)
+{
+    if(!b) return a;
+    return gcd(b,a%b);
+}
+ 
+int isprime(int n)
+{
+    if(n == 1) return 0;
+ 
+    for(int i=2;i*i<=n;i++) 
+    {
+        if(n%i == 0) return 0;
+    }
+    return 1;
+}
+
+void compute()
+{
+
+}
+
+void solve()
+{   
+    int n; cin >> n;
+    int ans = 0;
+
+    vector <pair<int, int>> points;
+    points.push_back({0, 0});
+    int x = 0, y = 0;
+
+    map <char, pair<int, int>> mp;
+    mp['R'] = {0, 1};
+    mp['L'] = {0, -1};
+    mp['U'] = {1, 0};
+    mp['D'] = {-1, 0};
+
+    set <pair<int, int>> perimeter;
+
+    for(int i=0; i<n; i++)
+    {
+        char dir; cin >> dir;
+        int temp; cin >> temp;
+        string color; cin >> color;
+
+        for(int j=0; j<=temp; j++) perimeter.insert({x + mp[dir].first*j, y + mp[dir].second*j});
+
+        x += mp[dir].first * temp;
+        y += mp[dir].second * temp;
+
+        points.push_back({x, y});
+    }
+
+    // Shoelace Theorem
+    int area = 0;
+    for(int i=1; i<points.size(); i++)
+    {
+        area += ((points[i].first * points[(i+1)%(n+1)].second) - (points[i].second * points[(i+1)%(n+1)].first));
+    }
+  
+    // Pick's Theorem
+    // A = i + b/2 - 1
+    // i = A - b/2 + 1
+
+    // ans = i + b = A + b/2 + 1
+    area = area*0.5;
+    cout << area + perimeter.size()/2 + 1<< '\n';
+}
+
+signed main(void) 
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    // cin >> t;
+
+    compute();
+    
+    for(int i=0;i<t;i++)
+    {
+        // cout << "Case #" << i+1 << ": "; // Kick Start
+        solve();
+    }
+
+	return 0;
+}
